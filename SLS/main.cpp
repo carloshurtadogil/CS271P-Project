945f18d9310dbdce670bdf597c0fa0800a4b3eb1
syk <1600012967@pku.edu.cn>
2020-12-08 00:37:00 +0800

    SA（not finished）

--- SLS/main.cpp
+++ SLS/main.cpp
@@ -1,15 +1,36 @@
 #include "../Common/Node.h"
-#include <iostream> 
+#include <iostream>
+#include <vector>
 
 // g++ SLS/main.cpp Common/Node.cpp 
 
 using namespace std;
 
-int main() {
-  Node n1 ("Irvine");
-  Node n2 ("Santa Ana");
-  n1.setParent(n2);
-  n1.setLocation("Anaheim");
-  cout << "Parent of "<< n1.getLocation() << " is " <<n1.getParent()->getLocation()<< endl;
-  return 0;
+int n;
+typedef vector<float> VEC;
+vector<VEC> e;
+
+float evaluation(const VEC &x) {
+    float  ret = 0;
+    for(int i = 1; i < n; i++)
+        ret += e[x[i-1]][x[i]];
+    ret += e[x[n-1]][x[0]];
+    return ret;
+}
+
+VEC SA(const vector<VEC> &e, float  alpha) {
+    float T = 100;
+
+}
+
+int main(int argc, char **argv) {
+    FILE* file = fopen(argv[1], "r");
+    fscanf(file, "%d", &n);
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++) {
+            float x;
+            fscanf(file, "%f", &x);
+            e[i].push_back(x);
+        }
+    VEC ans = SA(e, 0.9);
 }
